1cab903a4736723eb6e83add116d71ee07bd828e
Amr Abouelmagd <amrakmal97@gmail.com>
2024-06-21 13:53:08 -0500

    rebasing and fixing conflict

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -11,6 +12,10 @@ hobbies = ["Football", "Video Games", "Watching Aurora"]
 work_experience = ["SWE @ Incorta     2021-2024", "SWE Intern @ M3ntorship      01/2021-04/2021"]
 education = ["M.Sc. @ TTU     2024-2026", "B.Sc. @ Alexandria University    2026-2021"]
 
-@app.route('/')
+@app.route('/', endpoint='index')
 def index():
     return render_template('index.html', education=education, work_experience=work_experience, hobbies=hobbies, title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies', endpoint='hobbies')
+def hobbies():
+    return render_template('hobbies.html', hobbies=hobbies_arr, url=os.getenv("URL"))
