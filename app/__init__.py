5f85484a187584a00d1b273d28adc309c0904def
caosl <caosl@uci.edu>
2024-07-24 13:36:16 -0700

    add tests and fix errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, render_template, request, jsonify
+import os, re
+from flask import Flask, make_response, jsonify, render_template, request
 from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader
 from app.utils import User
@@ -41,20 +41,16 @@ mydb.create_tables([TimelinePost])
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    data=request.get_json()
-    if not data:
-        return jsonify({"error": "Invalid"}), 400
-    print(data)
-    name=data.get('name')
-    email=data.get('email')
-    content=data.get('content')
+    data = request.form
+    name, email, content = data.get('name'), data.get('email'), data.get('content')
 
     if not name:
-        return jsonify(error="Invalid name"), 400
+        return make_response(jsonify({'error': 'Invalid name'}), 400)
+    if not email or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return make_response(jsonify({'error': 'Invalid email'}), 400)
     if not content:
-        return jsonify(error="Invalid content"), 400
-    if '@' not in email:
-        return jsonify(error="Invalid email"), 400
+        return make_response(jsonify({'error': 'Invalid content'}), 400)
+    
 
     timeline_post=TimelinePost.create(name=name, email=email, content=content)
 
